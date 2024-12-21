d9ee8adc2c32727775922d95560b6046ec776e23
Mohit Bindal <themohitbindal@gmail.com>
2024-12-21 14:48:19 +0530

    shifted content

--- src/app/screen/page.tsx
+++ src/app/screen/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function ScreenPage() {
   return (
-    <div>ScreenPage</div>
-  )
+    <div>
+      <div className="border border-b-2 p-2 w-full font-bold text-xl">
+        SCREEN
+      </div>
+      <div className="flex">
+        <div className="flex w-full">
+          <div className="m-5 w-">
+            <div className="flex justify-between">
+              <div className="">Locations</div>
+              <div>Add Locations</div>
+            </div>
+          </div>
+          {/* <div className=" w-full">
+            <div className="flex ">
+              <div>Screens</div>
+              <div>add screens</div>
+            </div>
+          </div> */}
+        </div>
+        <div>Settings</div>
+      </div>
+    </div>
+  );
 }
